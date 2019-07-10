a7b32f3d1a996576ce2e19762d4da5f9cbb95a46
sandeepshiven <sandeepshiven0@gmail.com>
2019-07-10 23:39:03 +0530

    Create sets.py

---
+++ sets.py
@@ -0,0 +1,14 @@
+my_set = set()
+
+my_set.add(3)
+my_set.add(4)
+my_set.add(2)
+my_set.add(1)
+my_set.add(3)
+my_set.add(2)
+print(f"{my_set}")
+
+# or we can do this
+my_list = [1,2,2,244,411,11,1,1,222,3,3,3,3]
+my_set1 = set(my_list)
+print(f"{my_set1}")
