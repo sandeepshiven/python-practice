d8ad4c917c396579c27d99e0636800959137fea7
sandeepshiven <sandeepshiven0@gmail.com>
2019-08-25 15:01:39 +0530

    Update missing_num.py

--- bin/missing_num.py
+++ bin/missing_num.py
@@ -1,7 +1,8 @@
 from tkinter import * 
 root = Tk() 
-root.geometry('300x300') 
-root.title("First Tkinter Window") 
+root.geometry('300x30') 
+root.title("First Tkinter Window")
+root.bbox(column = 10, row = 10, col2 = 30,row2 = 30) 
 root.mainloop() 
 
 
