6354ca71999b2a472a4c60c821c3711fe206c1b8
sandeep shiven <sandeepshiven0@gmail.com>
2019-08-27 01:15:35 +0530

    date time incomplete

---
+++ advance python modules/datetime.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 26 19:37:19 2019
+
+@author: sandeep
+"""
+
+import datetime
+
+c_date = datetime.time.
+
+print(c_date)
+
+
+
+
+
+
+
+
+
+
