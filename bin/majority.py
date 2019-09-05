40a0925e7bfdcda695848f59356233c0ebfb7f5c
sandeep shiven <sandeepshiven0@gmail.com>
2019-09-05 21:49:07 +0530

    just random

--- bin/majority.py
+++ bin/majority.py
@@ -1,39 +1,13 @@
-#!/bin/python3
-
-import math
-import os
-import random
-import re
-import sys
-
-# Complete the arrayManipulation function below.
-def arrayManipulation(n, queries):
-    arr = []
-    for i in range(n):
-        arr.append(0)
-    for j in range(len(queries)):
-        add = queries[j][2]
-        for k in range(queries[j][0]-1,queries[j][1]):
-            arr[k] += add
-    retrun max(arr)
-
-
-if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    nm = input().split()
-
-    n = int(nm[0])
-
-    m = int(nm[1])
-
-    queries = []
-
-    for _ in range(m):
-        queries.append(list(map(int, input().rstrip().split())))
-
-    result = arrayManipulation(n, queries)
-
-    fptr.write(str(result) + '\n')
-
-    fptr.close()
+class a():
+    value = 0
+    def __init__(self,num):
+        self.num = num
+        print(self.num)
+    print(value)
+    
+class b():
+    value = 2
+    num = 6
+    print(value)
+    print(num)
+    
