8910f3adca0332341bd2c737656018c42d0227f6
sandeep shiven <sandeepshiven0@gmail.com>
2019-09-08 22:49:21 +0530

    advanced numbers

---
+++ advance objects and data structures/numbers.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  8 15:46:21 2019
+
+@author: sandeep
+"""
+
+
+print(hex(155757))
+print(bin(123))
+print(pow(3,4))
+print(pow(3,4,5))
+print(abs(-3.14))
+print(round(15,-1))
+print(round(3.1415926535,5))
+
+
+
+
+
+
+
+
+
+
+
