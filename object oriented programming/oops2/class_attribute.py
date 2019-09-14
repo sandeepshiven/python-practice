9eea46c829964843fad22d9f274d360f071662f0
sandeep shiven <sandeepshiven0@gmail.com>
2019-09-14 18:57:47 +0530

    class attributes in python

--- object oriented programming/oops2/class_attribute.py
+++ object oriented programming/oops2/class_attribute.py
@@ -1,9 +1,15 @@
 class User:
-    def __init__(self,first,last,age): # this init method is always called first on 
-        self.first = first             # instantiating a class
-        self.last = last              # self is used to create a seperate variable
-        self.age = age                #or method for each instance 
-    
+    active_users = 0  # class attribute it defined once and shared between all instances of the same class
+    def __init__(self,first,last,age): 
+        self.first = first             # self variabels are only restricted to particular class instance
+        self.last = last              # and cannot be used without object.variable
+        self.age = age             
+        User.active_users += 1   # syntax for using class attribute
+
+    def logout(self):
+        User.active_users -= 1
+        return f"{self.first} has logged out"
+
     def full_name(self):
         return f"My name is {self.first} {self.last}"
     
@@ -26,21 +32,28 @@ class User:
 user1 = User('Sandeep','Shiven',20)
 user2 = User('Ansh','Shrivastava',68)
 
-print(user1.full_name())
-print(user2.full_name())
-print(user1.initials())
-print(user2.initials())
-print(user1.likes('coding'))
-print(user2.likes('chatting'))
-print(user1.is_senior())
-print(user2.is_senior())
-print(user1.age)
-print(user1.birthday())
-print(user1.age)
+print(User.active_users)
 
-print(user2.age)
-print(user2.birthday())
-print(user2.age)
+print(user2.logout())
+del user2 #deleting user2 instance
+print(User.active_users)
+print(user2.first)
+
+# print(user1.full_name())
+# print(user2.full_name())
+# print(user1.initials())
+# print(user2.initials())
+# print(user1.likes('coding'))
+# print(user2.likes('chatting'))
+# print(user1.is_senior())
+# print(user2.is_senior())
+# print(user1.age)
+# print(user1.birthday())
+# print(user1.age)
+
+# print(user2.age)
+# print(user2.birthday())
+# print(user2.age)
 
 
 
