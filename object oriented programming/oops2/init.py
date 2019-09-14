1bf9210b4e1b1ecc1ce3afa31d6643ec87fce4c2
sandeep shiven <sandeepshiven0@gmail.com>
2019-09-14 13:53:46 +0530

    learning oops again

--- object oriented programming/oops2/init.py
+++ object oriented programming/oops2/init.py
@@ -1,8 +1,8 @@
 class User:
     def __init__(self,first,last,age): # this init method is always called first on 
         self.first = first             # instantiating a class
-        self.last = last
-        self.age = age
+        self.last = last              # self is used to create a seperate variable
+        self.age = age                #or method for each instance
 
 
 user1 = User('Sandeep','Shiven',20)
