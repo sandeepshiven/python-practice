31a560e84e884affb0736edf60897cd6b1cb44a7
sandeep shiven <sandeepshiven0@gmail.com>
2019-09-21 11:52:19 +0530

    iterables and iterators example

---
+++ iterators and generators/itrables_iterators.py
@@ -0,0 +1,28 @@
+''' iterator = an object that can be iterated upon. An object which returns data,one element at a time when next() 
+    is called on it
+
+    iterable = an object which will return an iterator when iter() is called on it 
+'''
+string = 'lkjsflgaslg'
+try:
+    print(next(string))
+except:
+    print("TypeError: 'str' object is not an iterator")
+string = iter(string)
+print(next(string))
+print(next(string))
+print(next(string))
+print(next(string))
+
+l = [1,4,6,2.3,4,1,6,3.2,4,87,8,63,3.4,4.6,4]
+try:
+    print(next(l))
+except:
+    print("TypeError: 'list' object is not an iterator")
+
+l = iter(l)
+print(next(l))
+print(next(l))
+print(next(l))
+print(next(l))
+print(next(l))
--- test.py
+++ test.py
@@ -1,7 +1,3 @@
-a = [1,2,3,0]
-b = set(a)
-b = list(b)
-
-b.sort()
-
-print(b)
+from collections import Counter
+l = [1,11,1,1,2,22,2,2,2,2,9,9,8,56,6,28,5,2,6,5,2,6,8,6,3,4,6,4]
+print(Counter(l))
