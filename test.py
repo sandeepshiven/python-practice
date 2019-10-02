2cae5413fb9356865ea7004f9b2ddc9899a9babb
sandeep shiven <sandeepshiven0@gmail.com>
2019-10-02 18:45:16 +0530

    ignore

--- test.py
+++ test.py
@@ -1,5 +1,18 @@
-while True:
-    temp = int(input())
-    if temp == 42:
-        break
-    print(temp)
+
+def my_sort(arr):
+    new_arr = []
+    for i in arr:
+        if i[1].split('@')[1] == 'gmail.com':
+            new_arr.append(i[0])
+    return(sorted(new_arr))
+
+
+fptr = open('input08.txt','r')
+test = int(fptr.readline())
+arr = []
+for i in range(test):
+    arr.append(tuple(map(str ,fptr.readline().split())))
+#print(arr)
+arr = my_sort(arr)
+for i in arr:
+    print(i)
