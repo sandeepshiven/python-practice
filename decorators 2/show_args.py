3757c6f9cb12d5c5b8dd5f12b0251053aee76cc3
sandeep shiven <sandeepshiven0@gmail.com>
2019-10-02 18:40:21 +0530

    showing the *args and **kwargs using decorators

--- decorators 2/show_args.py
+++ decorators 2/show_args.py
@@ -1,15 +1,17 @@
-from itertools import wraps
+from functools import wraps
 
-def show_args(fun):
-    @wraps(fun)
+def show_args(fn):
+    @wraps(fn)
     def wrapper(*args,**kwargs):
-        result = fun(*args,**kwargs)
+        result = fn(*args,**kwargs)
         print(f"Here are the args:{result[0]}")
         print(f"Here are the kargs:{result[1]}")
+    return wrapper
 
 
 @show_args
 def do_nothing(*args, **kwargs):
+    return [tuple(args),dict(kwargs)]
     
 
 do_nothing(1, 2, 3,a="hi",b="bye")
