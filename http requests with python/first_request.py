c2b8430034b17bf2f87f5346f5db9ca9a1b5cedb
sandeep shiven <sandeepshiven0@gmail.com>
2019-10-07 20:41:12 +0530

    first http request

--- http requests with python/first_request.py
+++ http requests with python/first_request.py
@@ -5,4 +5,4 @@ url = "http://www.google.com/"
 response = requests.get(url)
 
 print(f"Your request to {url} came back with status code {response.status_code}")
-print(response.text)
+print(response.text) #prints all the html code
