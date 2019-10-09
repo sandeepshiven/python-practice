5f9564b1f02087b2d70b17c6cc966f41a2a825af
sandeep shiven <sandeepshiven0@gmail.com>
2019-10-10 00:45:21 +0530

    finding a word and replacing it in a file

---
+++ file handeling/find_replace.py
@@ -0,0 +1,20 @@
+def find_replace(filename, word, replacement):
+    with open('test.txt','r') as file:
+        words = file.read()
+        for i in range(len(words)):
+            flag = 0
+            temp = ''
+            if words[i] == word[0]:
+                for j in range(len(word)):
+                    if words[i+j] == word[j]:
+                        temp += word[j]
+                    else:
+                        break
+                if temp == word:
+                    flag = 1
+            if flag:
+                words = words[:i] + replacement +words[i+len(word):]
+    with open('test.txt','w') as file:
+        file.write(words)
+
+find_replace('test.txt','Alice', "Sandeep")
