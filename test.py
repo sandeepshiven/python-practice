d9d6639e85b9527e92d9a3292dff624a79647204
sandeep shiven <sandeepshiven0@gmail.com>
2019-12-09 13:27:49 +0530

    scrapping project

--- test.py
+++ test.py
@@ -1,16 +1,29 @@
+from itertools import permutations
+from collections import Counter
+
+def main():
+    test = int(input())
+    for i in range(test):
+        a = input()
+        b = input()
+        a=set(permutations(a,len(a)))
+        b=set(permutations(b,len(b)))
+        a = set(map(str_to_dec,a))
+        b = set(map(str_to_dec,b))
+        l = [i^j for i in a for j in b]
+        print(len(set(l)))
+
+def str_to_dec(l):
+    two = 0
+    num = 0
+    for i in l:
+        num += int(i)*(2**two)
+        two += 1
+    return num
+
+if __name__=="__main__":
+    main()
 
 
-import itertools
 
-def possibleWords(a,N):
-    ##Your code jkldefhere
-    keypad = {2:"abc",3:"def",4:"ghi",5:"jkl",6:"mno",7:"pqrs",8:"tuv",9:"wxyz"}
-    l = [keypad[i] for i in a]
-    l1 = itertools.product(*l)
-    for i in l1:
-        print(*i,sep='',end=' ')
-
-
-
-
-possibleWords([2,3,4],3)
+    
